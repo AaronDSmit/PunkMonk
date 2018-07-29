52df39cac800da76495f6c3397b4eb2c74c5de56
AaronDSmit <smitaaron1@gmail.com>
2018-07-29 12:37:54 +1000

    Add several new management scripts
    Intro menu deals with transitioning from the splash screen into the game. Manager deals with loading all the manager scripts before starting game play. Turn manager deals with the turn based gameplay. UI manager deals with all the UI.

---
+++ PunkMonkPrototype/Assets/Scripts/Monobehaviours/Management/Manager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    // Global Manager ref
+    public static Manager instance;
+
+    private StateManager stateManager;
+    private UIManager uiManager;
+    private TransitionManager transitionManager;
+    private TurnManager turnManager;
+
+    public StateManager StateController
+    {
+        get { return stateManager; }
+    }
+
+    public UIManager UIController
+    {
+        get { return uiManager; }
+    }
+
+    public TransitionManager TransitionController
+    {
+        get { return transitionManager; }
+    }
+
+    public TurnManager TurnController
+    {
+        get { return turnManager; }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+
+            stateManager = GetComponent<StateManager>();
+            transitionManager = GetComponent<TransitionManager>();
+            uiManager = GetComponent<UIManager>();
+            turnManager = GetComponent<TurnManager>();
+        }
+        else
+        {
+            //stop the game from having more than one GameStateManager
+            Destroy(gameObject);
+            return;
+        }
+
+        //Don't destroy the GameStateManager in scene
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        StartCoroutine(InitManagers());
+    }
+
+    private IEnumerator InitManagers()
+    {
+        uiManager.Init();
+
+        yield return new WaitUntil(() => uiManager.Ready);
+
+        stateManager.Init();
+
+        yield return new WaitUntil(() => stateManager.Ready);
+
+        turnManager.Init();
+
+        yield return new WaitUntil(() => turnManager.Ready);
+
+        transitionManager.Init();
+
+        yield return new WaitUntil(() => transitionManager.Ready);
+
+        stateManager.StartGame();
+    }
+}
---
+++ PunkMonkPrototype/Assets/Scripts/Monobehaviours/Management/UIManager.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private Button move;
+
+    private Button attack;
+
+    private Button specialAttack;
+
+    private GameObject gameplayUI;
+
+    private Image fadePlane;
+
+    private Text loadingText;
+
+    private PlayerController player;
+
+    private Unit selectedUnit;
+
+    private Button[] buttons;
+
+    private bool[] buttonInitialState;
+
+    private bool isLocked;
+
+    private bool isReady;
+
+    public void Init()
+    {
+        isReady = true;
+
+        //isLocked = false;
+
+        //GameObject playerGO = GameObject.FindGameObjectWithTag("Player");
+
+        //if (playerGO)
+        //{
+        //    player = playerGO.GetComponent<PlayerController>();
+        //}
+        //else
+        //{
+        //    Debug.LogError("No player controller found!");
+        //}
+
+        //buttons = GetComponentsInChildren<Button>();
+
+        //buttonInitialState = new bool[buttons.Length];
+
+        //for (int i = 0; i < buttons.Length; i++)
+        //{
+        //    buttonInitialState[i] = buttons[i].interactable;
+        //}
+
+        //
+    }
+
+    private void Awake()
+    {
+        Transform canvas = GameObject.FindGameObjectWithTag("UI").transform;
+
+        if (canvas)
+        {
+            gameplayUI = canvas.GetChild(0).gameObject;
+
+            move = gameplayUI.transform.GetChild(0).GetComponent<Button>();
+            attack = gameplayUI.transform.GetChild(1).GetComponent<Button>();
+            specialAttack = gameplayUI.transform.GetChild(2).GetComponent<Button>();
+
+            fadePlane = canvas.Find("FadePlane").GetComponent<Image>();
+            loadingText = fadePlane.transform.GetComponentInChildren<Text>();
+
+            if (fadePlane)
+            {
+                fadePlane.color = new Color(fadePlane.color.r, fadePlane.color.g, fadePlane.color.b, 1.0f);
+            }
+
+            if (loadingText)
+            {
+                loadingText.color = new Color(loadingText.color.r, loadingText.color.g, loadingText.color.b, 1.0f);
+            }
+
+            gameplayUI.SetActive(false);
+        }
+
+        TurnManager.TurnEvent += TurnEvent;
+
+        // Subscribe to game state
+        StateManager.OnGameStateChanged += GameStateChanged;
+    }
+
+    private void GameStateChanged(Game_state a_oldstate, Game_state a_newstate)
+    {
+        // Leaving the loading state
+        if (a_oldstate == Game_state.loading)
+        {
+            StartCoroutine(FadeOutLoading());
+        }
+
+        if (a_newstate == Game_state.battle)
+        {
+            StartCoroutine(FadeIntoBattle());
+        }
+        else
+        {
+            gameplayUI.SetActive(false);
+        }
+    }
+
+    private IEnumerator FadeIntoBattle()
+    {
+        StartCoroutine(FadeImage(0, 1, 1.0f));
+
+        yield return new WaitForSeconds(1.0f);
+
+        gameplayUI.SetActive(true);
+
+        StartCoroutine(FadeImage(1, 0, 1.0f));
+    }
+
+    private IEnumerator FadeOutLoading()
+    {
+        //yield return new WaitForSeconds(0.5f);
+
+        // fade out from black
+        StartCoroutine(FadeImage(1, 0, 0.5f));
+        StartCoroutine(Fade(Color.white, Color.clear, 0.5f, loadingText));
+
+        yield return null;
+    }
+
+    // Fades the fadePlane image from a colour to another over x seconds.
+    private IEnumerator FadeImage(float from, float to, float time)
+    {
+        float currentLerpTime = 0;
+        float t = 0;
+
+        while (t < 1)
+        {
+            currentLerpTime += Time.deltaTime;
+            t = currentLerpTime / time;
+
+            fadePlane.color = new Color(fadePlane.color.r, fadePlane.color.g, fadePlane.color.b, Mathf.Lerp(from, to, t));
+
+            yield return null;
+        }
+    }
+
+    // Fades the fadePlane image from a colour to another over x seconds.
+    private IEnumerator Fade(Color from, Color to, float time, Text _text)
+    {
+        float speed = 1 / time;
+        float percent = 0;
+
+        while (percent < 1)
+        {
+            percent += Time.deltaTime * speed;
+            _text.color = Color.Lerp(from, to, percent);
+
+            yield return null;
+        }
+    }
+
+    public void SelectAction(int actionIndex)
+    {
+        // player.SelectAction(actionIndex);
+    }
+
+    private void TurnEvent(Turn_state a_newState, TEAM a_team, int a_turnNumber)
+    {
+        if (a_team == TEAM.player)
+        {
+            if (a_newState == Turn_state.start)
+            {
+                //ToggleHUD();
+                //endTurnButton.Toggle();
+            }
+            else if (a_newState == Turn_state.end)
+            {
+                //ToggleHUD();
+                //endTurnButton.Toggle();
+            }
+        }
+    }
+
+    public bool Ready
+    {
+        get { return isReady; }
+    }
+}

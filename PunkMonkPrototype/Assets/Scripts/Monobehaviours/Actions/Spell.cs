75f8ff470c3238c94a59b34e9d96e36c9fd84445
Jezzalittle <jezzalittle20@gmail.com>
2018-07-29 12:56:06 +1000

    added: BasicEarthAttack
    changed the spells to have a castSpell function

--- PunkMonkPrototype/Assets/Scripts/Monobehaviours/Actions/Spell.cs
+++ PunkMonkPrototype/Assets/Scripts/Monobehaviours/Actions/Spell.cs
@@ -1,8 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+
+/// <summary>
+/// Base class for any spell that a character can cast:
+/// 
+/// 1) Object must belong to a Spell Prefab.
+/// 2) Object must implement A Cast Spell Function that takes in an array of Tiles and a onStart and OnFinish callback.
+///
+/// </summary>
+
 
 public class Spell : MonoBehaviour
 {
-    
+    protected virtual void CastSpell(Tile[] a_tiles, System.Action a_onStartCB, System.Action a_onFinishCB)
+    {
+        a_onStartCB();
+
+    }
 }
---
+++ PunkMonkPrototype/Assets/Scripts/Monobehaviours/Actions/Spells/BasicEarthAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// The earth character basic spell is a :
+/// 
+/// 1) Object must belong to a Spell Prefab.
+/// 2) Object must call the base class's CastSpell fuction.
+///
+/// </summary>
+
+public class BasicEarthAttack : Spell
+{
+    [SerializeField] float damageAmount;
+
+    protected override void CastSpell(Tile[] a_tile, System.Action a_onStartCB, System.Action a_onFinishCB)
+    {
+        base.CastSpell(a_tile, a_onStartCB, a_onFinishCB);
+        foreach (Tile hex in a_tile)
+        {
+            if (hex.CurrentUnit != null)
+            {
+                hex.CurrentUnit.TakeDamage(Element.EARTH, damageAmount);
+            }
+        }
+    }
+
+}

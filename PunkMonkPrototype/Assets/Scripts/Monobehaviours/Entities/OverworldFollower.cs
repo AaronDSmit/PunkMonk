00f6d7f6ec1226649b5b95d17e430fdd507a4478
Jeremy Little <jezzalittle20@gmail.com>
2018-07-30 14:41:31 +1000

    add OverworldFollower to any charicater so it can follow the player in the overworld

---
+++ PunkMonkPrototype/Assets/Scripts/Monobehaviours/Entities/OverworldFollower.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverworldFollower : MonoBehaviour
+{
+
+    private Unit earthUnit;
+    private bool inOverworld;
+    private Vector3 vecBetween;
+
+    [SerializeField] private float movementSpeed;
+    
+    private void Awake()
+    {
+        StateManager.OnGameStateChanged += GameStateChanged;
+        Init();
+    }
+
+    public void Init()
+    {
+        GameObject earthGO = GameObject.FindGameObjectWithTag("EarthUnit");
+
+        if (earthGO)
+        {
+            earthUnit = GameObject.FindGameObjectWithTag("EarthUnit").GetComponent<Unit>();
+        }
+        else
+        {
+            Debug.LogError("No Earth unit found!");
+        }
+    }
+
+    private void GameStateChanged(Game_state _oldstate, Game_state _newstate)
+    {
+        // ensure this script knows it's in over-world state
+        inOverworld = (_newstate == Game_state.overworld);
+    }
+
+    private void Update()
+    {
+        if (inOverworld)
+        {
+            if (Vector3.Distance(transform.position, earthUnit.transform.position) > 2)
+            {
+                vecBetween = earthUnit.transform.position - transform.position;
+                transform.position += vecBetween.normalized * movementSpeed * Time.deltaTime;
+            }
+        }
+    }
+
+}

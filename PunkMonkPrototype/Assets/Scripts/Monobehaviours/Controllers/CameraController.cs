4581c7ed42945db32131f2af96ed0e3743b5297b
Jeremy Little <jezzalittle20@gmail.com>
2018-07-31 09:56:00 +1000

    Update CameraController to use phyiscs based movement instead of lerping

--- PunkMonkPrototype/Assets/Scripts/Monobehaviours/Controllers/CameraController.cs
+++ PunkMonkPrototype/Assets/Scripts/Monobehaviours/Controllers/CameraController.cs
@@ -16,9 +16,15 @@ public class CameraController : MonoBehaviour
 
     private bool lockedMovement = false;
 
-    [SerializeField] private float lerpSpeed;
+
+    private Vector3 vel;
+
+    [SerializeField] private float slowSpeed;
+    [SerializeField] private float acculturationSpeed;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float overworldSpeed;
     [SerializeField] private float RotationalSpeed;
-    [SerializeField] private float cameraSpeed;
+    [SerializeField] private float scrollSpeed;
 
 
     private void Awake()
@@ -52,7 +58,7 @@ public class CameraController : MonoBehaviour
             ProcessKeyboardInput();
         }
 
-        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * lerpSpeed);
+        //  transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * lerpSpeed);
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * RotationalSpeed);
 
     }
@@ -74,36 +80,47 @@ public class CameraController : MonoBehaviour
     // Process Keyboard Input
     private void ProcessKeyboardInput()
     {
+        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-        if (Input.GetAxis("Vertical") > 0)
-        {
-            targetPos += transform.forward * cameraSpeed * Time.deltaTime;
-        }
+        if(vel.magnitude < maxSpeed)
+        vel += dir * acculturationSpeed * Time.deltaTime;        
+        transform.position += transform.forward * vel.z;
+        transform.position += transform.right * vel.x;
 
-        if (Input.GetAxis("Horizontal") < 0)
-        {
-            targetPos += -transform.right * cameraSpeed * Time.deltaTime;
-        }
 
-        if (Input.GetAxis("Horizontal") > 0)
+        if (dir == Vector3.zero)
         {
-            targetPos += transform.right * cameraSpeed * Time.deltaTime;
+            vel *= slowSpeed;
         }
+        //if (Input.GetAxis("Vertical") > 0)
+        //{
+        //    targetPos += transform.forward * cameraSpeed * Time.deltaTime;
+        //}
 
-        if (Input.GetAxis("Vertical") < 0)
-        {
-            targetPos += -transform.forward * cameraSpeed * Time.deltaTime;
-        }
+        //if (Input.GetAxis("Horizontal") < 0)
+        //{
+        //    targetPos += -transform.right * cameraSpeed * Time.deltaTime;
+        //}
+
+        //if (Input.GetAxis("Horizontal") > 0)
+        //{
+        //    targetPos += transform.right * cameraSpeed * Time.deltaTime;
+        //}
+
+        //if (Input.GetAxis("Vertical") < 0)
+        //{
+        //    targetPos += -transform.forward * cameraSpeed * Time.deltaTime;
+        //}
 
 
 
         if (Input.GetAxis("Mouse ScrollWheel") > 0)
         {
-            targetPos += -Vector3.up * cameraSpeed * Time.deltaTime;
+            targetPos += -Vector3.up * scrollSpeed * Time.deltaTime;
         }
         if (Input.GetAxis("Mouse ScrollWheel") < 0)
         {
-            targetPos += Vector3.up * cameraSpeed * Time.deltaTime;
+            targetPos += Vector3.up * scrollSpeed * Time.deltaTime;
         }
 
 
@@ -129,5 +146,6 @@ public class CameraController : MonoBehaviour
         {
             targetPos = earthUnit.transform.position;
         }
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * overworldSpeed);
     }
 }

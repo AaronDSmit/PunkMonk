039ffc149ea6de705553719cdfb8ea702fe52bac
Jeremy Little <jezzalittle20@gmail.com>
2018-08-27 14:26:48 +1000

    Add AudioManager

---
+++ PunkMonkPrototype/Assets/Scripts/Monobehaviours/Management/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] music;
+
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (music.Length > 0)
+        {
+            audioSource.clip = music[0];
+        }
+        audioSource.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

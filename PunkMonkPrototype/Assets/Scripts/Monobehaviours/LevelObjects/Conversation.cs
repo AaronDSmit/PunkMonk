8ae956b561287c0c4e94bff6e56d1fa324463d35
AaronDSmit <smitaaron1@gmail.com>
2018-09-13 19:24:24 +1000

    Added Conversation as it's own script

---
+++ PunkMonkPrototype/Assets/Scripts/Monobehaviours/LevelObjects/Conversation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conversation : ScriptableObject
+{
+    public new string name = "Conversation";
+    public List<Speach> dialogue = new List<Speach>();
+}
+
+[System.Serializable]
+public class Speach
+{
+    public enum Speaker { Clade, Gen };
+
+    public Speaker speaker = Speaker.Clade;
+
+    public string text;
+
+    public Speach()
+    {
+        speaker = Speaker.Clade;
+        text = "";
+    }
+
+    public Speach(Speaker a_speaker)
+    {
+        speaker = a_speaker;
+        text = "";
+    }
+}
--- PunkMonkPrototype/Assets/Scripts/Monobehaviours/Management/Dialogue.cs
+++ PunkMonkPrototype/Assets/Scripts/Monobehaviours/Management/Dialogue.cs
@@ -163,31 +163,3 @@ public class Dialogue : MonoBehaviour
 
     #endregion
 }
-
-public class Conversation : ScriptableObject
-{
-    public new string name = "Conversation";
-    public List<Speach> dialogue = new List<Speach>();
-}
-
-[System.Serializable]
-public class Speach
-{
-    public enum Speaker { Clade, Gen };
-
-    public Speaker speaker = Speaker.Clade;
-
-    public string text;
-
-    public Speach()
-    {
-        speaker = Speaker.Clade;
-        text = "";
-    }
-
-    public Speach(Speaker a_speaker)
-    {
-        speaker = a_speaker;
-        text = "";
-    }
-}

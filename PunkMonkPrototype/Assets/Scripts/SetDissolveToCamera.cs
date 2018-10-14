a85ee524d37fe86ce5616f08d56ef091d6b8764f
Gene <genecollis12@gmail.com>
2018-10-15 00:04:38 +1100

    Added a Dissolve shader

---
+++ PunkMonkPrototype/Assets/Scripts/SetDissolveToCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetDissolveToCamera : MonoBehaviour
+{
+    private Camera camera;
+    private Material material;
+
+	void Awake()
+    {
+        camera = Camera.main;
+        material = GetComponent<Renderer>().material;
+	}
+	
+	void Update ()
+    {
+        material.SetVector("_RayDirection", camera.transform.forward);
+        material.SetVector("_RayOrigin", camera.transform.position);
+    }
+}

dfd6c2b94d42728324fa0c31166e6015cc7f9d59
Gene Collis <genecollis12@gmail.com>
2018-10-16 14:49:29 +1100

    Spinny thing

---
+++ PunkMonkPrototype/Assets/AnimationSpeedRandomizer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSpeedRandomizer : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator = null;
+
+    [SerializeField]
+    private float min = 0.01f;
+    [SerializeField]
+    private float max = 100f;
+
+    void Start()
+    {
+        animator.speed = Random.Range(min, max);
+
+        Destroy(this);
+    }
+}

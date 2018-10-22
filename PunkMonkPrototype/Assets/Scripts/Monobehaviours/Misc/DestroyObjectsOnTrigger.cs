33ad181a8ab96f0822011cfe4e6ded8fd54bf03c
Jeremy Little <jezzalittle20@gmail.com>
2018-10-22 15:05:02 +1100

    Add a utility script for destory objects on trigger enter

---
+++ PunkMonkPrototype/Assets/Scripts/Monobehaviours/Misc/DestroyObjectsOnTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectsOnTrigger : MonoBehaviour {
+
+	[SerializeField] GameObject[] objects;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		foreach (GameObject go in objects) 
+		{
+			Destroy (go);
+		}
+	}
+
+}

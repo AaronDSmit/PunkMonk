118b859db2ede1ae1ba2469a0211bbb301347ec3
Gene Collis <genecollis12@gmail.com>
2018-10-23 14:12:04 +1100

    Shader forge meta data

---
+++ PunkMonkPrototype/Assets/Scripts/Monobehaviours/Misc/DestroyOnTimer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DestroyOnTimer : MonoBehaviour
+{
+    [Header("Destroys the attached GameObject after time")]
+    [SerializeField]
+    private float time = 1f;
+    
+	void Awake()
+    {
+        Destroy(gameObject, time);
+	}
+	
+}

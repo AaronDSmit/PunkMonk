cf2eaed314cc20b698e615d5704e5f9997164980
Gene Collis <genecollis12@gmail.com>
2018-10-30 13:50:15 +1100

    Added SetMaterialColorToImage.cs

---
+++ PunkMonkPrototype/Assets/Scripts/Monobehaviours/Misc/SetMaterialColorToImage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class SetMaterialColorToImage : MonoBehaviour
+{
+    private Image image = null;
+
+    private void OnEnable()
+    {
+        image = GetComponent<Image>();
+    }
+
+    private void LateUpdate()
+    {
+        Color color = image.material.GetColor("_Colour");
+        color.a = image.color.a;
+        image.material.SetColor("_Colour", color);
+    }
+
+}

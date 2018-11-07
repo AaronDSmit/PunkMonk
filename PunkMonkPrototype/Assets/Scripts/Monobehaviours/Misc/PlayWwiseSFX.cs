867c0a2d3063b6ef8702b412515e1de0d7e892d4
Jeremy Little <jezzalittle20@gmail.com>
2018-11-07 13:42:44 +1100

    Add menu SFX script

---
+++ PunkMonkPrototype/Assets/Scripts/Monobehaviours/Misc/PlayWwiseSFX.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayWwiseSFX : MonoBehaviour
+{
+    [SerializeField] private AK.Wwise.Event soundSFX;
+
+
+    public void PlaySFX()
+    {
+        soundSFX.Post(gameObject);
+    }
+
+}

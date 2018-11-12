1c8f187da0935108dd45c5b1c61d8ff62c8a0ebb
Jordan Dines <jordan@dines.net.au>
2018-11-12 12:47:31 +1100

    Add credits

---
+++ PunkMonkPrototype/Assets/Scripts/Monobehaviours/UI/Credits.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour {
+
+
+    [SerializeField] private Animation creditsRollAnim;
+    [SerializeField] private GameObject buttonsPanel;
+    [SerializeField] private Transform creditsPanel;
+    [SerializeField] private float endPositionY;
+    [SerializeField] private bool isPlaying;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (creditsRollAnim.isPlaying)
+        {
+            isPlaying = true;
+            if (creditsPanel.position.y >= endPositionY)
+            {
+                isPlaying = false;
+            }
+        }
+
+        if (!isPlaying)
+        {
+            this.gameObject.SetActive(false);
+            buttonsPanel.SetActive(true);
+        }
+
+	}
+
+
+
+}

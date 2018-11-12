235d8521ce76a1a3a2a32d5e6a54663d18d93be5
Gene Collis <genecollis12@gmail.com>
2018-11-13 10:09:16 +1100

    Made the missile units snimation start before the missile is spawned

--- PunkMonkPrototype/Assets/Scripts/Monobehaviours/Entities/EnemyUnits/MissileUnit.cs
+++ PunkMonkPrototype/Assets/Scripts/Monobehaviours/Entities/EnemyUnits/MissileUnit.cs
@@ -14,7 +14,8 @@ public class MissileUnit : AI_Agent
     [SerializeField]
     private GameObject missilePrefab = null;
 
-    [SerializeField] private float missileSpawnDelay;
+    [SerializeField]
+    private float missileSpawnDelay = 1f;
 
     private int turns = 0;
 
@@ -92,12 +93,13 @@ public class MissileUnit : AI_Agent
     {
         doneMissiles = false;
 
+        animator.SetTrigger("BasicAttack");
+
         yield return new WaitForSeconds(missileSpawnDelay);
 
         GameObject missileGO = Instantiate(missilePrefab, projectilePosition.position, missilePrefab.transform.rotation);
         Missile missile = missileGO.transform.GetChild(0).GetComponent<Missile>();
         missile.TriggerUp();
-        animator.SetTrigger("BasicAttack");
 
         yield return new WaitForSeconds(0.1f);
         yield return new WaitUntil(() => missile.Done);
